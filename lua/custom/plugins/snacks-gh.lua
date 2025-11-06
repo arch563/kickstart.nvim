ed7d2a8f67675587766ed98a2412cbcfebe4cc33
arch563 <archiepullan@gmail.com>
2025-11-06 22:21:27 +0000

    starting migration

--- lua/custom/plugins/snacks-gh.lua
+++ lua/custom/plugins/snacks-gh.lua
@@ -7,6 +7,40 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		},
+		dashboard = {
+			sections = {
+				{ section = "header" },
+				{
+					pane = 2,
+					section = "terminal",
+					cmd = "colorscript -e square",
+					height = 5,
+					padding = 1,
+				},
+				{ section = "keys", gap = 1, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
+			},
+			-- your dashboard configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+
 		picker = {
 			sources = {
 				gh_issue = {
@@ -26,4 +60,5 @@ return {
 		{ "<leader>gp", function() Snacks.picker.gh_pr() end,                     desc = "GitHub Pull Requests (open)" },
 		{ "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end,    desc = "GitHub Pull Requests (all)" },
 	},
+
 }
